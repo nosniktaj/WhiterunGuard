390579c1b7ea4199de6c461c0307e5e13df8294d
nosniktaj <nosniktaj@icloud.com>
2024-12-16 16:57:54 +0000

    General code rework

--- WhiterunGuard/Program.cs
+++ WhiterunGuard/Program.cs
@@ -3,120 +3,131 @@ using Discord.Net;
 using Discord.WebSocket;
 using Newtonsoft.Json;
 
-namespace WhiterunGuard;
-
-internal class Program
+namespace WhiterunGuard
 {
-    private static DiscordSocketClient _client = null!;
-    private static TikTokHandler _tikTokHandler = null!;
-
-    private static readonly string _discordToken =
-        Environment.GetEnvironmentVariable("WHITERUN_DISCORD_TOKEN") ?? string.Empty;
-
-    private static readonly bool _running = true;
-
-    public static async Task Main()
+    internal class Program
     {
-        await startClient();
+        private static DiscordSocketClient _client = null!;
+        private static TikTokHandler _tikTokHandler = null!;
 
-        while (_running)
+        private static IUser _adminUser = null!;
+
+        private static readonly string _discordToken =
+            Environment.GetEnvironmentVariable("WHITERUN_DISCORD_TOKEN") ?? string.Empty;
+
+        private static readonly bool _running = true;
+
+        public static async Task Main()
         {
-            //Console Control Code
+            await startClient();
+
+            while (_running)
+            {
+                var command = Console.ReadLine();
+                switch (command.ToLowerInvariant())
+                {
+                    case "end":
+                        Console.WriteLine("> Are you sure you want to end the program? (Y/N)");
+                        break;
+                }
+                //Console Control Code
+            }
         }
-    }
 
-    #region Private Static Methods
+        #region Custom Events
 
-    private static async Task startClient()
-    {
-        if (string.IsNullOrWhiteSpace(_discordToken))
+        private static async void TikTokLiveStarted(object? sender, EventArgs e)
         {
-            Console.WriteLine("No valid Discord Token has been provided.");
-        }
-        else
-        {
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
-            _client.Ready += Client_Ready;
-            _client.SlashCommandExecuted += SlashCommandHandler;
-
-            await _client.LoginAsync(TokenType.Bot, _discordToken);
-            await _client.StartAsync();
-        }
-    }
-
-    #endregion
-
-    #region Custom Events
-
-    private static async void TikTokLiveStarted(object? sender, EventArgs e)
-    {
 #if DEBUG
-        await _client.GetUserAsync(617471240667398154).Result
+        var x = await _client.GetUserAsync(617471240667398154).Result
             .SendMessageAsync("Lyla is now live on TikTok! \n https://www.tiktok.com/@lylaskyrim/live");
 #else
-                await  _client.GetGuild(1205836076187394079).GetTextChannel(1205836076728451104).SendMessageAsync("@everyone Lyla is now live on TikTok! \n https://www.tiktok.com/@lylaskyrim/live");
+            await _client.GetGuild(1205836076187394079).GetTextChannel(1205836076728451104)
+                .SendMessageAsync("@everyone Lyla is now live on TikTok! \n https://www.tiktok.com/@lylaskyrim/live");
 #endif
-    }
-
-    #endregion
-
-    #region Standard Events
-
-    private static async Task Client_Ready()
-    {
-        _tikTokHandler = new();
-        _tikTokHandler.LiveStarted += TikTokLiveStarted;
-
-        var guild = _client.GetGuild(1205836076187394079);
-        var sayCommand = new SlashCommandBuilder();
-        sayCommand.WithName("say");
-        sayCommand.WithDescription("Say a message");
-        sayCommand.AddOption("message", ApplicationCommandOptionType.String, "The message you want to say",
-            true);
-        await guild.DeleteApplicationCommandsAsync();
-        try
-        {
-            await guild.CreateApplicationCommandAsync(sayCommand.Build());
         }
-        catch (HttpException ex)
+
+        #endregion
+
+        #region Private Static Methods
+
+        private static async Task startClient()
         {
-            var json = JsonConvert.SerializeObject(ex.Errors, Formatting.Indented);
-            Console.WriteLine(json);
+            if (string.IsNullOrWhiteSpace(_discordToken))
+            {
+                Console.WriteLine("No valid Discord Token has been provided.");
+            }
+            else
+            {
+                _client = new DiscordSocketClient();
+                _client.Log += Log;
+                _client.Ready += Client_Ready;
+                _client.SlashCommandExecuted += SlashCommandHandler;
+
+                await _client.LoginAsync(TokenType.Bot, _discordToken);
+                await _client.StartAsync();
+            }
         }
-    }
 
-    private static Task Log(LogMessage arg)
-    {
-        Console.WriteLine(arg.Message);
-        return Task.CompletedTask;
-    }
+        #endregion
 
-    #endregion
+        #region Standard Events
 
-    #region Commands
-
-    private static async Task SlashCommandHandler(SocketSlashCommand command)
-    {
-        switch (command.Data.Name)
+        private static async Task Client_Ready()
         {
-            case "say":
-                await Say(command);
-                break;
+            _tikTokHandler = new();
+            _tikTokHandler.LiveStarted += TikTokLiveStarted;
+
+            var guild = _client.GetGuild(1205836076187394079);
+            var sayCommand = new SlashCommandBuilder();
+            sayCommand.WithName("say");
+            sayCommand.WithDescription("Say a message");
+            sayCommand.AddOption("message", ApplicationCommandOptionType.String, "The message you want to say",
+                true);
+            sayCommand.WithDefaultMemberPermissions(GuildPermission.Administrator);
+            try
+            {
+                await guild.CreateApplicationCommandAsync(sayCommand.Build());
+            }
+            catch (HttpException ex)
+            {
+                var json = JsonConvert.SerializeObject(ex.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
         }
-    }
 
-    private static async Task Say(SocketSlashCommand command)
-    {
-        var message = (string)command.Data.Options.First().Value;
-        var sender = (SocketGuildUser)command.User;
-        if (sender is not null && (sender.GuildPermissions.Administrator ||
-                                   sender.Roles.Any(r =>
-                                       r.Name.Equals("Lyla's Boyfriend", StringComparison.OrdinalIgnoreCase))))
-            await command.RespondAsync(message);
-        else
-            await command.RespondAsync("Sorry, you don't have permission to use this command.");
-    }
+        private static Task Log(LogMessage arg)
+        {
+            Console.WriteLine($" {arg.Message}");
+            _ = Console.ReadLine();
+            Console.Write("> ");
+            return Task.CompletedTask;
+        }
 
-    #endregion
+        #endregion
+
+        #region Commands
+
+        private static async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "say":
+                    await Say(command);
+                    break;
+            }
+        }
+
+        private static async Task Say(SocketSlashCommand command)
+        {
+            var message = (string)command.Data.Options.First().Value;
+            var sender = (SocketGuildUser)command.User;
+            if (sender.IsAdmin())
+                await command.RespondAsync(message);
+            else
+                await command.RespondAsync("Sorry, you don't have permission to use this command.");
+        }
+
+        #endregion
+    }
 }
--- WhiterunGuard/StaticExtensions.cs
+++ WhiterunGuard/StaticExtensions.cs
@@ -1,7 +1,14 @@
+using Discord.WebSocket;
+
 namespace WhiterunGuard
 {
     public static class StaticExtensions
     {
-        public static DateTime TimeAccurateToMinutes(this DateTime dt) => new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+        public static DateTime TimeAccurateToMinutes(this DateTime dt) =>
+            new(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+
+        public static bool IsAdmin(this SocketGuildUser? user) => user is not null &&
+                                                                  (user.GuildPermissions.Administrator ||
+                                                                   user.Id == 6174712406673981542);
     }
 }
